151de4f7410c2d63dbc6a9ffc827cb4f07964eec
g.alekseev <gleb1alekseev@gmail.com>
2025-04-02 19:38:32 +0300

    initial commit

--- src/main/java/headhunter_objects/Vacancy.java
+++ src/main/java/headhunter_objects/Vacancy.java
@@ -1,4 +1,20 @@
 package headhunter_objects;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
 public class Vacancy {
+    @Expose
+    String name;
+    @Expose
+    Salary salary;
+    @SerializedName("alternate_url")
+    @Expose
+    String alternateUrl;
+
 }
